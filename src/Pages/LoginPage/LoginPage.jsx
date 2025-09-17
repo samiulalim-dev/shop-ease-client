95373e51fadb9b5f97fce3d889b3f599d05e2596
samiulalim-dev <samiulalim.dev@gmail.com>
2025-09-17 22:39:08 +0600

    login functionality completed

--- src/Pages/LoginPage/LoginPage.jsx
+++ src/Pages/LoginPage/LoginPage.jsx
@@ -1,11 +1,38 @@
-import React from "react";
+import React, { use, useState } from "react";
 import Lottie from "lottie-react";
 import loginAnimation from "../../assets/lottieFiles/login.json";
-import { Link } from "react-router";
-import { FcGoogle } from "react-icons/fc";
+import { Link, useNavigate } from "react-router";
 import GoogleSignin from "../../Shared/GoogleSignin/GoogleSignin";
+import { AuthContext } from "../../AuthProvider/AuthProvider";
+import { useForm } from "react-hook-form";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import { toast } from "react-toastify";
 
 const LoginPage = () => {
+  const [seePassword, setSeePassword] = useState(true);
+  const { signInUser } = use(AuthContext);
+  const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = async (data) => {
+    // console.log(data);
+    const email = data.email;
+    const password = data.password;
+    signInUser(email, password)
+      .then((result) => {
+        // console.log(result);
+        toast.success("Login successfully!");
+        navigate("/");
+        reset();
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   return (
     <div className="min-h-screen py-16 flex flex-col md:flex-row items-center justify-center bg-gray-50 px-4 sm:px-8 lg:px-16">
       {/* LEFT - Form */}
@@ -13,7 +40,7 @@ const LoginPage = () => {
         <h2 className="text-2xl sm:text-3xl font-bold text-[#003F62] mb-6">
           Welcome Back
         </h2>
-        <form className="space-y-5">
+        <form onSubmit={handleSubmit(onSubmit)} className="space-y-5">
           {/* Email */}
           <div>
             <label className="block text-sm font-medium text-gray-700 mb-1">
@@ -21,21 +48,49 @@ const LoginPage = () => {
             </label>
             <input
               type="email"
+              {...register("email", { required: "Email is required" })}
               placeholder="Enter your email"
               className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-[#EDA415]"
             />
+            {errors.email && (
+              <p className="text-red-500 text-sm">{errors.email.message}</p>
+            )}
           </div>
 
           {/* Password */}
-          <div>
+          <div className="relative">
             <label className="block text-sm font-medium text-gray-700 mb-1">
               Password
             </label>
             <input
-              type="password"
+              type={seePassword ? "password" : "text"}
+              {...register("password", {
+                required: "Password is required",
+
+                minLength: {
+                  value: 6,
+                  message: "Password must be at least 6 characters",
+                },
+              })}
               placeholder="Enter your password"
               className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-[#EDA415]"
             />
+
+            <button
+              onClick={() => setSeePassword(!seePassword)}
+              type="button"
+              className=" absolute cursor-pointer top-9 right-3"
+            >
+              {seePassword ? (
+                <FaEye size={20}></FaEye>
+              ) : (
+                <FaEyeSlash size={20}></FaEyeSlash>
+              )}
+            </button>
+
+            {errors.password && (
+              <p className="text-red-500 text-sm">{errors.password.message}</p>
+            )}
           </div>
 
           {/* Remember + Forgot */}
