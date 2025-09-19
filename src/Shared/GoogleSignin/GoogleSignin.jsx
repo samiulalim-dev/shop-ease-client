66e87bdd141a7b64feaacc0a47bf2918396e61c8
samiulalim-dev <samiulalim.dev@gmail.com>
2025-09-20 01:26:58 +0600

    when user login by google user data save to DB done

--- src/Shared/GoogleSignin/GoogleSignin.jsx
+++ src/Shared/GoogleSignin/GoogleSignin.jsx
@@ -3,16 +3,35 @@ import { FcGoogle } from "react-icons/fc";
 import { AuthContext } from "../../AuthProvider/AuthProvider";
 import { FaCheckCircle, FaTimesCircle } from "react-icons/fa";
 import { toast } from "react-toastify";
+import useAxios from "../../Hooks/useAxios/useAxios";
+import { useNavigate } from "react-router";
 
 const GoogleSignin = () => {
   const { googleSignin } = use(AuthContext);
+  const axiosInstance = useAxios();
+  const navigate = useNavigate();
   const handleGoogleSignin = () => {
     googleSignin()
       .then((result) => {
         console.log(result);
+        axiosInstance
+          .post("/user", {
+            name: result.user.displayName,
+            email: result.user.email,
+            photo: result.user.photoURL,
+            role: "user",
+            createdAt: new Date(),
+          })
+          .then((res) => {
+            // console.log(res.data);
+          })
+          .catch((error) => {
+            console.log("Failed to save user to DB:", error);
+          });
         toast.success("Login successful!", {
           icon: <FaCheckCircle className="text-green-500" />,
         });
+        navigate("/");
       })
       .catch((error) => {
         console.log(error);
